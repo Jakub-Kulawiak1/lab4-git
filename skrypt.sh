de16ba76479396edc0e82dd4461aeab7ce9f27a2
Jakub <dsw56044@student.dsw.edu.pl>
2025-06-30 23:01:11 +0200

    Dodano parametr liczby plików do --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,7 @@
-if [[ "$1" == "--logs" ]]; then
-    for i in {1..100}; do
+if [[ "$1" == "--logs" && -n "$2" ]]; then
+    for i in $(seq 1 "$2"); do
         echo "Plik $i" > "log${i}.txt"
     done
 fi
+
 

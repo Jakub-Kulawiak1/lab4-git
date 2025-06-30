de9604c5ee794076e00d5fab37ff643b378b58ae
Jakub <dsw56044@student.dsw.edu.pl>
2025-06-30 23:06:15 +0200

    Dodano wersje skrócone flag -h, -1, -d

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,16 @@
-if [[ "$1" == "--help" ]]; then
-    echo "--date       wyświetla datę"
-    echo "--logs [N]   tworzy N plików log"
-    echo "--help       pokazuje pomoc"
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "--date / -d     wyświetla datę"
+    echo "--logs / -l     tworzy pliki log"
+    echo "--help / -h     pomoc"
+elif [[ "$1" == "--date" || "$1" == "-d" ]]; then
+    date
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+    N=${2:-100}
+    for i in $(seq 1 "$N"); do
+        echo "Plik $i" > "log${i}.txt"
+    done
 fi
+
 
 
 

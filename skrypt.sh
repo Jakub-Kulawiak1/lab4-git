141efe71f91b07ecc75cf79558411b69a1acffa0
Jakub <dsw56044@student.dsw.edu.pl>
2025-06-30 23:08:22 +0200

    Dodano flagę --init do klonowania i ustawienia PATH

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,8 @@
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    echo "--date / -d     wyświetla datę"
-    echo "--logs / -l     tworzy pliki log"
-    echo "--help / -h     pomoc"
-elif [[ "$1" == "--date" || "$1" == "-d" ]]; then
-    date
-elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
-    N=${2:-100}
-    for i in $(seq 1 "$N"); do
-        echo "Plik $i" > "log${i}.txt"
-    done
+if [[ "$1" == "--init" ]]; then
+    git clone https://github.com/TWOJ-LOGIN/TWOJE-REPO.git temp_repo
+    export PATH=$PATH:$(pwd)/temp_repo
 fi
+
 
 
 

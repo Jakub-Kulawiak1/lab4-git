f7f0fdf76c7b645e79d6adfda32c250fb48743eb
Jakub-Kulawiak1 <qbedan@gmail.com>
2025-06-30 23:21:22 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" == "--init" ]]; then
-    git clone https://github.com/TWOJ-LOGIN/TWOJE-REPO.git temp_repo
+    git clone https://github.com/Jakub-Kulawiak1/lab4-git.git temp_repo
     export PATH=$PATH:$(pwd)/temp_repo
 fi
 
